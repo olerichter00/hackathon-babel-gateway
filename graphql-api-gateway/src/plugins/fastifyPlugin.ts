ade0ce4ae9eb67ec6ad30a0c5d75e013d9afea50
Ole Richter <ole.richter91@gmail.com>
2020-12-18 13:54:11 +0100

    change header name

--- graphql-api-gateway/src/plugins/fastifyPlugin.ts
+++ graphql-api-gateway/src/plugins/fastifyPlugin.ts
@@ -5,8 +5,9 @@ const DEFAULT_LANGUAGE = 'en'
 
 function translationPlugin(instance, options, next) {
   instance.addHook('onSend', async (request, reply, payload) => {
+    const myRequest = request
     const locale =
-      (request.headers['Accept-Language'] && request.headers['Accept-Language'].slice(0, 2)) ||
+      (request.headers['accept-language'] && request.headers['accept-language'].slice(0, 2)) ||
       DEFAULT_LANGUAGE
 
     const translatedPayload = await translator(JSON.parse(payload), SOURCE_LANGUAGE, locale)
