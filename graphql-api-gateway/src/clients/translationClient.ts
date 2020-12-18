a3fbeab86cc03ca645c5a6426ae50931528f5451
Ole Richter <ole.richter91@gmail.com>
2020-12-18 15:53:15 +0100

    Update translationClient.ts

--- graphql-api-gateway/src/clients/translationClient.ts
+++ graphql-api-gateway/src/clients/translationClient.ts
@@ -1,7 +1,7 @@
 const translatte = require('translatte');
 
 const translate = async (msg: string, source= "de", target = "en"): Promise<string> => {
-    return await translatte(msg, { from: source, target: target }).then(res => {
+    return await translatte(msg, { from: source, to: target }).then(res => {
         return res.text
     }).catch(err => {
         throw console.error(err)
