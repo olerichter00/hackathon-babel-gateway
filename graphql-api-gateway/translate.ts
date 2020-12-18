f9145b1ad6861aa11c1c7d56a5e011f2f6bc645b
Ricardo Trindade <ricardo.trindade743@gmail.com>
2020-12-18 10:56:36 +0000

    Create translate.ts

---
+++ graphql-api-gateway/translate.ts
@@ -0,0 +1,20 @@
+const { AWSTranslateJSON } = require('aws-translate-json');
+
+const awsConfig = {
+    accessKeyId: 'akid',
+    secretAccessKey: 'secret',
+    region: 'eu-west-1',
+}
+
+const source = "de";
+const taget = ["en"];
+
+const { translateJSON } = new AWSTranslateJSON(awsConfig, source, taget);
+
+const translate = () => {
+  translateJSON({
+    key1: "Wasser bitte",
+}).then(console.log);
+}
+
+export default translate
