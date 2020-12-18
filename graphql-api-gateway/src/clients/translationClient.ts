d964214ad5a5a4a57538627124e6f04b112a557f
Ricardo Trindade <ricardo.trindade743@gmail.com>
2020-12-18 12:27:24 +0000

    Update translationClient.ts

--- graphql-api-gateway/src/clients/translationClient.ts
+++ graphql-api-gateway/src/clients/translationClient.ts
@@ -1,1 +1,20 @@
-const translatate = async (name, fromLanguage, toLanguage) => `${toLanguage}: ${name}
+//const translatate = async (name, fromLanguage, toLanguage) => `${toLanguage}: ${name}
+
+const { AWSTranslateJSON } = require('aws-translate-json');
+
+const awsConfig = {
+    accessKeyId: '...',
+    secretAccessKey: 'D..',
+    region: 'eu-west-1',
+}
+
+const translate = (recipe: string, source= "de", target = "en") => {
+  const { translateJSON } = new AWSTranslateJSON(awsConfig, source, [target]);
+  translateJSON({
+    key1: recipe,
+}).then(res => {
+  return res["target"]
+});
+}
+
+export default translate
